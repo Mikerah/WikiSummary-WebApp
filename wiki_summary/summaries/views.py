255276761cead308c5aa52cd4631a7c86c4a2621
Mikerah <mikerah14@gmail.com>
2016-08-17 19:12:36 -0400

    Create model and added javascript code that doesn't work to index.html

--- wiki_summary/summaries/views.py
+++ wiki_summary/summaries/views.py
@@ -7,6 +7,9 @@ def index(request):
     if request.method == 'POST':
         form = ArticleForm(request.POST)
         
+        if form.is_valid():
+            selection = form.cleaned_data['options']
+        
     else:
         form = ArticleForm()
     return render(request, 'summaries/index.html', {'form': form})
