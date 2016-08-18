d1daeba44cec4ba0810623c390f9e653ab4ec1b9
Mikerah <mikerah14@gmail.com>
2016-08-18 11:44:13 -0400

    finished index view in views.py

--- wiki_summary/summaries/views.py
+++ wiki_summary/summaries/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 from .forms import ArticleForm
 from .models import Article
+
+import wikipedia
 
 def index(request):
     if request.method == 'POST':
@@ -9,7 +12,17 @@ def index(request):
         
         if form.is_valid():
             selection = form.cleaned_data['options']
-        
+            if selection == '1':
+                spec_article = form.cleaned_data['specific_article']
+                request.session['wanted_article'] = spec_article[:]
+                return HttpResponseRedirect('/article.html')
+            elif selection == '2':
+                number_of_art = form.cleaned_data['number_of_random_articles']
+                request.session['number_of_articles'] = number_of_art[:]
+                return HttpResponseRedirect('/articles.html')
+            else:
+                return HttpResponseRedirect('/infinite_articles.html')
+                
     else:
         form = ArticleForm()
     return render(request, 'summaries/index.html', {'form': form})
