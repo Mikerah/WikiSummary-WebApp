1fcd44769de1f6aeda1dfab1e404208d6406c058
Mikerah <mikerah14@gmail.com>
2016-08-18 11:03:47 -0400

    modified index.html so that when a selection is press a form appears and did some very basic styling

--- wiki_summary/summaries/forms.py
+++ wiki_summary/summaries/forms.py
@@ -3,6 +3,6 @@ from django import forms
 class ArticleForm(forms.Form):
     CHOICES = [(1, 'Read an article'), (2, 'Read n random articles'), 
                 (3, 'Read random articles until I\'m bored')]
-    options = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect(attrs={'id': 'options'}))
-    specific_article = forms.CharField(max_length=250, required=False, widget=forms.TextInput(attrs={'id': 'spec_art'}))
-    number_of_random_articles = forms.IntegerField(max_value=10, required=False, widget=forms.NumberInput(attrs={'id': 'num_art'}))
+    options = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect(attrs={'id': 'selections'}))
+    specific_article = forms.CharField(max_length=250, required=False, widget=forms.TextInput(attrs={}))
+    number_of_random_articles = forms.IntegerField(max_value=10, required=False, widget=forms.NumberInput(attrs={}))
