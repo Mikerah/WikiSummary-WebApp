00238b08479265dd1c5d02065bb99ef551c15823
Mikerah <mikerah14@gmail.com>
2016-08-18 21:34:24 -0400

    Finished styling for index.html

--- wiki_summary/summaries/forms.py
+++ wiki_summary/summaries/forms.py
@@ -3,5 +3,5 @@ from django import forms
 class ArticleForm(forms.Form):
     CHOICES = [(1, 'Read an article'), (2, 'Read n random articles')]
     options = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect(attrs={'id': 'selections'}))
-    specific_article = forms.CharField(max_length=250, required=False, widget=forms.TextInput(attrs={}))
-    number_of_random_articles = forms.IntegerField(max_value=10, required=False, widget=forms.NumberInput(attrs={}))
+    specific_article = forms.CharField(max_length=250, required=False, widget=forms.TextInput(), label=("Specific Article"))
+    number_of_random_articles = forms.IntegerField(required=False, widget=forms.NumberInput(), label=("Number of Articles"))
